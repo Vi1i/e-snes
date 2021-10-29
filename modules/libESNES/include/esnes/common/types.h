52e5f8e8e5e8de3498a8384e6cf40878ebac361d
Vili <vili.petal@protonmail.com>
2021-10-29 15:13:22 -0500

    Format and local vimrc

--- modules/libESNES/include/esnes/common/types.h
+++ modules/libESNES/include/esnes/common/types.h
@@ -2,10 +2,12 @@
 #define ESNES_COMMON_TYPES_H
 
 #include <cstdint>
+
 namespace esnes::common::types {
 using byte = std::uint8_t;
 using signed_byte = std::int8_t;
 using word = std::uint16_t;
 using signed_word = std::int16_t;
 }  // namespace esnes::common::types
+
 #endif  // ESNES_ESNES_H
