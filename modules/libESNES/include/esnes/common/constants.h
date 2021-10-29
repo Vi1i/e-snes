37aeedfebaed95097c0787a0daaa55bc07400ab9
Vili <vili.petal@protonmail.com>
2021-10-29 15:02:42 -0500

    Format fix

--- modules/libESNES/include/esnes/common/constants.h
+++ modules/libESNES/include/esnes/common/constants.h
@@ -2,10 +2,8 @@
 #define ESNES_COMMON_CONSTANTS_H
 
 #include <cstdint>
-namespace esnes::common {
-namespace constants {
+namespace esnes::common::constants {
 static constexpr const auto smc_header_size{512u};
 static constexpr const auto rom_block_size{1024u};
-}  // namespace constants
-}  // namespace esnes::common
+}  // namespace esnes::common::constants
 #endif  // ESNES_ESNES_H
--- modules/libESNES/include/esnes/common/types.h
+++ modules/libESNES/include/esnes/common/types.h
@@ -2,12 +2,10 @@
 #define ESNES_COMMON_TYPES_H
 
 #include <cstdint>
-namespace esnes::common {
-namespace types {
+namespace esnes::common::types {
 using byte = std::uint8_t;
 using signed_byte = std::int8_t;
 using word = std::uint16_t;
 using signed_word = std::int16_t;
-}  // namespace types
-}  // namespace esnes::common
+}  // namespace esnes::common::types
 #endif  // ESNES_ESNES_H
